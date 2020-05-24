c9b76540e0891370ac449e3e6a44cb154eda5074
unknown <n20gaming2000@gmail.com>
2020-05-24 15:31:02 +0800

    fix indents

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    }
 ]
 
 const router = new VueRouter({ routes })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
-    <h1>Welcome to Azur Lane Toolbox</h1>
-  </div>
+    <div class="home">
+        <h1>Welcome to the Azur Lane Command Room</h1>
+    </div>
 </template>
