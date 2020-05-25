570ff6883476cedd893c9b9d4b210ce0cc23d432
unknown <n20gaming2000@gmail.com>
2020-05-25 09:26:51 +0800

    use publicPath instead

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    baseUrl: process.env.NODE_ENV === "production"
+    publicPath: process.env.NODE_ENV === "production"
         ? '/azur-lane-toolbox/'
-        :   '/',
+        :  '/',
     devServer: {
         proxy: "https://azur-lane-api.glitch.me/"
     }
