cc0491fd2d9a5102fff2973d160ed7849c42a560
unknown <n20gaming2000@gmail.com>
2020-05-26 16:05:40 +0800

    revert changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 import Home from "../views/Home.vue"
+import NotFound from "../views/NotFound.vue"
 
 Vue.use(VueRouter)
 const router = new VueRouter({
-    mode: "history",
     routes: [
         {
             path: "/",
             name: "Home",
             component: Home
+        },
+        {
+            path: "*",
+            name: "e404",
+            component: NotFound
         }
     ]
 })
