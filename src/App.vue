9661b6b669aaf99b823dc93046e46dda0a6912cd
unknown <n20gaming2000@gmail.com>
2020-05-27 08:11:44 +0800

    add basic navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <template>
     <div id="app">
+        <b-navbar type="is-primary">
+            <template slot="brand">
+                <b-navbar-item
+                    tag="router-link"
+                    :to="{ path: '/' }"
+                >
+                    <img
+                        src="./assets/images/icon.png"
+                        alt="Azur Lane Command Room"
+                    >
+                </b-navbar-item>
+            </template>
+            <template slot="start">
+                <b-navbar-item href="#">
+                    Home
+                </b-navbar-item>
+            </template>
+            <template slot="end">
+                <b-navbar-item tag="div">
+                    <b-button
+                        class="has-text-right"
+                        type="is-info"
+                        icon-left="heart"
+                        tag="a"
+                        href="https://ko-fi.com/W7W71CF9V"
+                        expanded
+                    />
+                </b-navbar-item>
+            </template>
+        </b-navbar>
         <router-view />
     </div>
 </template>
